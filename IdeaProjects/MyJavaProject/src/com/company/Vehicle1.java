9818e5aed7035ef568df9c656075d035c9822c3f
Shubhada <shubhadasanjay.paithankar@sjsu.edu>
2022-02-19 21:12:30 -0800

    simple kafka producer consumer program

--- IdeaProjects/MyJavaProject/src/com/company/Vehicle1.java
+++ IdeaProjects/MyJavaProject/src/com/company/Vehicle1.java
@@ -17,3 +17,14 @@ class Vinit1  {
         corolla.display();
     }
 }
+
+    git clone https://github.com/edenhill/kcat.git
+        Cloning into 'kcat'...
+        remote: Enumerating objects: 1550, done.
+        remote: Counting objects: 100% (280/280), done.
+        remote: Compressing objects: 100% (177/177), done.
+        remote: Total 1550 (delta 157), reused 195 (delta 93), pack-reused 1270
+        Receiving objects: 100% (1550/1550), 627.54 KiB | 620.00 KiB/s, done.
+        Resolving deltas: 100% (935/935), done.
+
+
